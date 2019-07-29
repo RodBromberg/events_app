bb3ce01380c3ff3da12d451ea0b23c134274a680
Rod <rod.bromberg@yahoo.com>
2019-07-29 18:15:40 -0400

    first commit

---
+++ events-application/src/app/layout/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
+export default App;
+
+//gi
